a62fec21516c663326344344deffe222c50271b2
Charles D. Randall <racinrandall@gmail.com>
2017-08-09 22:07:16 -0500

    part2 50%

--- src/finalProject/RPNCalculator.java
+++ src/finalProject/RPNCalculator.java
@@ -1,35 +1,37 @@
 package finalProject;
+
 import java.util.Stack;
 
 public class RPNCalculator {
+    String obj;
 
     abstract class Node {
-        private Node lhs;
-        private Node rhs;
+        Node lhs;
+        Node rhs;
 
         public Node() {
             lhs = rhs = null;
         }
 
         public abstract int valueOf();
-        public abstract String stringValueOf();
+
     }
 
-    class OperandNode extends Node(String lbl) {
+    class OperandNode extends Node {
         private String label;
+
         public OperandNode(String lbl) {
             super();
-            label=lbl;
+            label = lbl;
         }
+
         public int valueOf() {
             return Integer.valueOf(label);
         }
-        public String stringValueOf() {
-            return label;
-        }
+
     }
 
-    abstract class OperatorNode  extends Node {
+    abstract class OperatorNode extends Node {
         private String label;
         public OperatorNode(String lbl) {
             super();
@@ -38,57 +40,100 @@ public class RPNCalculator {
     }
 
     class PlusNode extends OperatorNode {
-        public PlusNode (String label) {
+        public PlusNode(String label) {
             super(label);
         }
+
         public int valueOf() {
             return lhs.valueOf() + rhs.valueOf();
         }
-        public String stringValueOf() {
-            return lhs.stringValueOf() " + " rhs.stringValueOf();
+
+    }
+
+    class MinusNode extends OperatorNode {
+        public MinusNode(String label) {
+            super(label);
+        }
+
+        public int valueOf() {
+            return lhs.valueOf() - rhs.valueOf();
         }
     }
 
-    class ExpTree {
-        private Node root;
-        public ExpTree() {
-            root = null;
+        class TimesNode extends OperatorNode {
+            public TimesNode(String label) {
+                super(label);
+            }
+
+            public int valueOf() {
+                return lhs.valueOf() * rhs.valueOf();
+            }
+
         }
-        public ExpTree(int val) {
-            root = new OperandNode(val);
+
+    class DivisionNode extends OperatorNode {
+        public DivisionNode(String label) {
+            super(label);
+        }
+
+        public int valueOf() {
+            return lhs.valueOf() / rhs.valueOf();
+        }
+
+    }
+    public boolean isOperand(Object symbol) {
+        if ((!"P".equals(symbol)) && (!"M".equals(symbol)) && (!"D".equals(symbol)) && (!"T".equals(symbol))) {
+            return true;
+        }
+        else {
+            return false;
         }
     }
 
+    public Node ExpTree(Stack S) {
+        Node root = null;
+        Node rhs;
+        Node lhs;
+        if (S.isEmpty()) {
+            return null;
+        } else {
+            obj = S.pop().toString();
+            if (isOperand(obj)) {
+                return new OperandNode(obj);
+            } else {
+                rhs = ExpTree(S);
+                lhs = ExpTree(S);
+                switch (obj) {
+                    case "P":
+                        return new PlusNode(obj);
+                    break;
+                    case "M":
+                        return new MinusNode(obj);
+                    break;
+                    case "T":
+                        return new TimesNode(obj);
+                    break;
+                    case "D":
+                        return new DivisionNode(obj);
+                    break;
+                }
+                root.lhs = lhs;
+                root.rhs = rhs;
+                return root;
 
-
+            }
+        }
+    }
 
 
     public static void main(String[] args) {
         if (args.length != 0) {
-            Stack S = new Stack();
+            Stack<String> S = new Stack<String>();
             for (String symbol : args) {
-                if ((!"P".equals(symbol)) && (!"M".equals(symbol)) && (!"D".equals(symbol)) && (!"T".equals(symbol))) {
-                    S.push(symbol);
-                } else {
-                    int rhs = Integer.parseInt(S.pop().toString());
-                    int lhs = Integer.parseInt(S.pop().toString());
-                    switch (symbol) {
-                        case "P":
-                            S.push(lhs + rhs);
-                            break;
-                        case "M":
-                            S.push(lhs - rhs);
-                            break;
-                        case "D":
-                            S.push(lhs / rhs);
-                            break;
-                        case "T":
-                            S.push(lhs * rhs);
-                            break;
-                    }
+                S.push(symbol);
                 }
             }
-            System.out.println(S.pop());
+
 
         }
 
