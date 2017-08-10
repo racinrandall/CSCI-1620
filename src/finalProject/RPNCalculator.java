399ad6dc854bc5aaebbda16a77aa4e8b72630a67
Charles D. Randall <racinrandall@gmail.com>
2017-08-10 18:40:38 -0500

    finished part2

--- src/finalProject/RPNCalculator.java
+++ src/finalProject/RPNCalculator.java
@@ -3,9 +3,9 @@ package finalProject;
 import java.util.Stack;
 
 public class RPNCalculator {
-    String obj;
 
-    abstract class Node {
+
+    static abstract class Node {
         Node lhs;
         Node rhs;
 
@@ -15,10 +15,15 @@ public class RPNCalculator {
 
         public abstract int valueOf();
 
+        public abstract String infixform();
+
+        public abstract String rpnform();
+
     }
 
-    class OperandNode extends Node {
-        private String label;
+    static class OperandNode extends Node {
+
+        String label;
 
         public OperandNode(String lbl) {
             super();
@@ -29,71 +34,110 @@ public class RPNCalculator {
             return Integer.valueOf(label);
         }
 
+        public String infixform() {
+            return label;
+        }
+
+        public String rpnform() {
+            return label;
+        }
+
     }
 
-    abstract class OperatorNode extends Node {
+    static abstract class OperatorNode extends Node {
         private String label;
-        public OperatorNode(String lbl) {
+
+        public OperatorNode() {
             super();
-            label = lbl;
         }
     }
 
-    class PlusNode extends OperatorNode {
-        public PlusNode(String label) {
-            super(label);
+    static class PlusNode extends OperatorNode {
+
+        public PlusNode() {
+            super();
         }
 
         public int valueOf() {
             return lhs.valueOf() + rhs.valueOf();
         }
 
+        public String infixform() {
+            return String.format("(%s + %s)", lhs.infixform(), rhs.infixform());
+        }
+
+        public String rpnform() {
+            return String.format("%s %s +", lhs.rpnform(), rhs.rpnform());
+        }
+
     }
 
-    class MinusNode extends OperatorNode {
-        public MinusNode(String label) {
-            super(label);
+    static class MinusNode extends OperatorNode {
+        public MinusNode() {
+            super();
         }
 
         public int valueOf() {
             return lhs.valueOf() - rhs.valueOf();
         }
-    }
 
-        class TimesNode extends OperatorNode {
-            public TimesNode(String label) {
-                super(label);
-            }
-
-            public int valueOf() {
-                return lhs.valueOf() * rhs.valueOf();
-            }
-
+         public String infixform() {
+            return String.format("(%s - %s)", lhs.infixform(), rhs.infixform());
         }
 
-    class DivisionNode extends OperatorNode {
-        public DivisionNode(String label) {
-            super(label);
+        public String rpnform() {
+            return String.format("%s %s -", lhs.rpnform(), rhs.rpnform());
+        }
+
+    }
+
+    static class TimesNode extends OperatorNode {
+        public TimesNode() {
+            super();
+        }
+
+        public int valueOf() {
+            return lhs.valueOf() * rhs.valueOf();
+        }
+        public String infixform() {
+            return String.format("(%s * %s)", lhs.infixform(), rhs.infixform());
+        }
+
+        public String rpnform() {
+            return String.format("%s %s *", lhs.rpnform(), rhs.rpnform());
+        }
+    }
+
+    static class DivisionNode extends OperatorNode {
+        public DivisionNode() {
+            super();
         }
 
         public int valueOf() {
             return lhs.valueOf() / rhs.valueOf();
         }
+        public String infixform() {
+            return String.format("(%s / %s)", lhs.infixform(), rhs.infixform());
+        }
 
+        public String rpnform() {
+            return String.format("%s %s /", lhs.rpnform(), rhs.rpnform());
+        }
     }
-    public boolean isOperand(Object symbol) {
+
+    public static boolean isOperand(Object symbol) {
         if ((!"P".equals(symbol)) && (!"M".equals(symbol)) && (!"D".equals(symbol)) && (!"T".equals(symbol))) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
 
-    public Node ExpTree(Stack S) {
+    static private Node ExpTree(Stack S) {
         Node root = null;
         Node rhs;
         Node lhs;
+        String obj;
         if (S.isEmpty()) {
             return null;
         } else {
@@ -105,22 +149,27 @@ public class RPNCalculator {
                 lhs = ExpTree(S);
                 switch (obj) {
                     case "P":
-                        return new PlusNode(obj);
-                    break;
+                        root = new PlusNode();
+                        root.lhs = lhs;
+                        root.rhs = rhs;
+                        break;
                     case "M":
-                        return new MinusNode(obj);
-                    break;
+                        root = new MinusNode();
+                        root.lhs = lhs;
+                        root.rhs = rhs;
+                        break;
                     case "T":
-                        return new TimesNode(obj);
-                    break;
+                        root = new TimesNode();
+                        root.lhs = lhs;
+                        root.rhs = rhs;
+                        break;
                     case "D":
-                        return new DivisionNode(obj);
-                    break;
+                        root = new DivisionNode();
+                        root.lhs = lhs;
+                        root.rhs = rhs;
+                        break;
                 }
-                root.lhs = lhs;
-                root.rhs = rhs;
                 return root;
-
             }
         }
     }
@@ -131,12 +180,17 @@ public class RPNCalculator {
             Stack<String> S = new Stack<String>();
             for (String symbol : args) {
                 S.push(symbol);
-                }
             }
-
+            Node tree = ExpTree(S);
+            System.out.println(tree.rpnform());
+            System.out.println(tree.infixform());
+            System.out.println(tree.valueOf());
 
         }
 
 
     }
+
+
 }
+
